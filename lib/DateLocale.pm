89909c2998c96367fbd31f79119ea36cd403298d
Николай Шуляковский <shulyakovskiy@mail.ru>
2014-08-23 20:19:33 +0000

    Import of NIKOLAS/DateLocale-1.17 from CPAN.
    gitpan-cpan-distribution: DateLocale
    gitpan-cpan-version:      1.17
    gitpan-cpan-path:         NIKOLAS/DateLocale-1.17.tar.gz
    gitpan-cpan-author:       NIKOLAS
    gitpan-cpan-maturity:     released

--- lib/DateLocale.pm
+++ lib/DateLocale.pm
@@ -5,7 +5,7 @@ use utf8;
 use POSIX qw/setlocale/;
 use Locale::Messages qw(:locale_h :libintl_h);
 use Encode;
-our $VERSION = '1.15';
+our $VERSION = '1.17';
 
 our $share_path = __FILE__;
 $share_path =~ s{\.pm$}{/share/locale};
